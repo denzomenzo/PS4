5b5fb03510af337fbe48fc4b8030c137a7ba499f
denzomenzo <denizsiringul69@gmail.com>
2025-12-17 20:35:03 +0000

    Update stripe.ts

--- demly-pos-salon/lib/stripe.ts
+++ demly-pos-salon/lib/stripe.ts
@@ -1,8 +1,5 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-11-20.acacia" as any,
-  typescript: true,
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
 
 export default stripe;
