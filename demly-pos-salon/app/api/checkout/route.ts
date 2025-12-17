69e5ecbbaa4194137a14ce0e69160ee7fb728e83
denzomenzo <denizsiringul69@gmail.com>
2025-12-17 14:09:50 +0000

    Update route.ts

--- demly-pos-salon/app/api/checkout/route.ts
+++ demly-pos-salon/app/api/checkout/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: '2025-12-15.clover',
+  
 });
 
 export async function POST(request: Request) {
@@ -91,4 +91,5 @@ export async function POST(request: Request) {
     );
   }
 }
+
 
