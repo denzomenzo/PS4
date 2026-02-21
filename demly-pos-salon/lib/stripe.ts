360d4d87bbdfdb9b3fdb18069bc74fa2b4b738fc
denzomenzo <denizsiringul69@gmail.com>
2026-02-21 13:23:35 +0000

    Update stripe.ts

--- demly-pos-salon/lib/stripe.ts
+++ demly-pos-salon/lib/stripe.ts
@@ -1,9 +1,7 @@
 // lib/stripe.ts
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2025-02-24.acacia", // Use latest stable version
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
 
 export const SUBSCRIPTION_PRICES = {
   monthly: process.env.STRIPE_PRICE_MONTHLY!,
